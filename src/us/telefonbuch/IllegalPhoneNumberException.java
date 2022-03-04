9fcd1f3e0aeb731b4777c38bb21d7480a23b8692
lstoudek <leonhard.stoudek@gmail.com>
2022-03-04 09:35:51 +0100

    bugfix PhoneNumber formated IllegalPhoneNumberException

--- src/us/telefonbuch/IllegalPhoneNumberException.java
+++ src/us/telefonbuch/IllegalPhoneNumberException.java
@@ -12,11 +12,24 @@ public class IllegalPhoneNumberException extends Throwable {
     public static int AREA_ILLEGAL = 1;
     public static int NUMBER_ILLEGAL = 2;
     public static int STRING_FORMAT_ILLEGAL = 3;
+    private final String ESCAPE = "\u001B[0m";
+    private final String RED = "\u001B[31m";
 
     IllegalPhoneNumberException(int type){
+        System.out.println(RED);
         this.type = type;
-
+        if (this.type == COUNTRY_ILLEGAL){
+            System.out.println("No valid countycode!");
+        }else if(this.type == AREA_ILLEGAL){
+            System.out.println("No valid Areacode!");
+        } else if (this.type == NUMBER_ILLEGAL){
+            System.out.println("No valid Number!");
+        } else if (this.type == STRING_FORMAT_ILLEGAL){
+            System.out.println("No valid String Format!");
+        }
+        System.out.println(ESCAPE);
     }
+
 
 
 
--- src/us/telefonbuch/PhoneNumber.java
+++ src/us/telefonbuch/PhoneNumber.java
@@ -25,14 +25,10 @@ public class PhoneNumber {
     PhoneNumber(String number) throws IllegalPhoneNumberException, CloneNotSupportedException{
         Pattern pattern = Pattern.compile("\\D");
         String[] s = number.split(pattern.pattern());
-        if (isValid(s[0].substring(1), s[1],  s[2])) {
-            if (s[0].contains("+")) {
-                this.country = Integer.parseInt(s[0].substring(1));
-            } else {
-                this.country = Integer.parseInt(s[0]);
-            }
-            this.areacode = Integer.parseInt(s[1]);
-            this.number = Integer.parseInt(s[2]);
+        if (isValid(s[1], s[2],  s[3])) {
+            this.country = Integer.parseInt(s[1]);
+            this.areacode = Integer.parseInt(s[2]);
+            this.number = Integer.parseInt(s[3]);
         }
     }
 
@@ -54,19 +50,20 @@ public class PhoneNumber {
     }
 
     public boolean isValid(String country, String areacode, String number ) throws IllegalPhoneNumberException{
-        try {
-            if (Integer.parseInt(country) >= 1000) {
+        boolean rv = true;
+        if (country.matches("[0-9]+"))
+
+        if (Integer.parseInt(country) >= 1000) {
+            rv = false;
                 throw new IllegalPhoneNumberException(IllegalPhoneNumberException.COUNTRY_ILLEGAL);
             }
             if (Integer.parseInt(areacode) >= 10000) {
+                rv = false;
                 throw new IllegalPhoneNumberException(IllegalPhoneNumberException.AREA_ILLEGAL);
             }if (Double.parseDouble(number) >= 2147483647){
+            rv = false;
                 throw new IllegalPhoneNumberException(IllegalPhoneNumberException.NUMBER_ILLEGAL);
             }
-
-        }catch (IllegalPhoneNumberException e){
-            System.out.println(e.type);
-        }
-        return true;
+            return rv;
     }
 }
